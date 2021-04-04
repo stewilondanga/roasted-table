6022dea27eccd68477143905fc71507484b9af56
Stewart ilondanga <stilondanga@gmail.com>
2021-04-04 15:25:37 +0300

    js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+/*var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+const editBtn = document.querySelector('.edit');
+const card = document.querySelector('.morph-card');
+let active = false;
+
+
+editBtn.addEventListener('click', e => {
+  e.preventDefault();
+  card.classList.toggle('active');
+  active = !active;
+  editBtn.innerText = active ? 'save' : 'edit';
+})
